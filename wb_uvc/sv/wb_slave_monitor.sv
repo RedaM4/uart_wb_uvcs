c6fa5af5a55450256100af753507b7efdd42fa1e
Abdulmalek <95189513+Abdulmalek-1@users.noreply.github.com>
2025-03-19 02:52:23 +0300

    Update wb_slave_monitor.sv

--- wb_uvc/sv/wb_slave_monitor.sv
+++ wb_uvc/sv/wb_slave_monitor.sv
@@ -2,43 +2,63 @@
 
 
 ///////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////
-//Abdulmalik please do this
+
+//Abdulmalik did this
 
 //////////////////////////////////////////////////////////
 class wb_slave_monitor extends uvm_monitor;
 
-`uvm_component_utils(wb_slave_monitor); 
+  `uvm_component_utils(wb_slave_monitor);
 
-function new(string name = "wb_slave_monitor", uvm_component parent);
-super.new(name,parent); 
-`uvm_info("--MONITOR_CLASS--","INSIDE CONSTRUCTOR",UVM_HIGH);
-endfunction
+  n_cpu_transaction trans; 
+  uvm_analysis_port#(n_cpu_transaction) analysis_port; 
+  virtual interface wb_slave_if vif; 
 
-function void start_of_simulation_phase(uvm_phase phase);
-super.start_of_simulation_phase(phase);
-`uvm_info("--MONITOR_CLASS--","START OF SIMULATION PHASE",UVM_HIGH);
-endfunction
+  function new(string name = "wb_slave_monitor", uvm_component parent);
+    super.new(name, parent);
+    `uvm_info("--SLAVE_MONITOR_CLASS--", "INSIDE CONSTRUCTOR", UVM_HIGH);
+  endfunction
 
-function void build_phase(uvm_phase phase);
-super.build_phase(phase);
-`uvm_info("--MONITOR_CLASS--","INSIDE BUILD PHASE",UVM_HIGH);
-endfunction
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    `uvm_info("--SLAVE_MONITOR_CLASS--", "INSIDE BUILD PHASE", UVM_HIGH);
 
+    analysis_port = new("analysis_port", this);
 
-function void connect_phase(uvm_phase phase);
-super.connect_phase(phase);
-`uvm_info("--MONITOR_CLASS--","INSIDE CONNECT PHASE",UVM_HIGH);
-endfunction
+    if(!(wb_vif_config::get(this,"","vif",vif)))begin
+        `uvm_error("SLAVE_MONITOR CLASS", "Failed to get vif from config db");
+    end
+  endfunction
 
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    `uvm_info("--SLAVE_MONITOR_CLASS--", "INSIDE RUN PHASE", UVM_HIGH);
 
-task run_phase(uvm_phase phase);
-super.run_phase(phase);
-`uvm_info("--MONITOR_CLASS--","INSIDE RUN PHASE",UVM_HIGH);
+    forever begin
+      @(posedge vif.clk);
 
-endtask
+      trans = n_cpu_transaction::type_id::create("trans");
+
+      if (vif.STB_I) begin
+        trans.address = vif.ADR_I;
+        trans.data    = vif.WE_I ? vif.DAT_I : vif.DAT_O; 
+        trans.M_STATE = vif.WE_I ? WRITE : READ;
+
+        while (!vif.ACK_O) begin
+          @(posedge vif.clk);
+        end
+      end else begin
+        trans.address = vif.ADR_I; 
+        trans.data    = 0;         
+        trans.M_STATE = IDLE;      
+      end
+
+      if (trans != null) begin
+        analysis_port.write(trans);
+        `uvm_info("--SLAVE_MONITOR_CLASS--", $sformatf("Captured Transaction: Address=0x%h, Data=0x%h, M_STATE=%s",
+                                                       trans.address, trans.data, trans.M_STATE.name()), UVM_HIGH);
+      end
+    end
+  endtask
 
 endclass: wb_slave_monitor
