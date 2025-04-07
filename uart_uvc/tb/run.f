9b8ba22128208732d4ad4215ed3d89bde262f1ea
AhmadAlroqi <ahmadyalroqi@gmail.com>
2025-04-07 14:05:57 +0300

    FB

--- uart_uvc/tb/run.f
+++ uart_uvc/tb/run.f
@@ -4,4 +4,6 @@
 
 +UVM_VERBOSITY=UVM_FULL 
 +ntb_random_seed_automatic
-+UVM_TESTNAME=base_test
+// +UVM_TESTNAME=base_test
++UVM_TESTNAME=uart_rand_dual_send_test
+// +UVM_TESTNAME=uart_parity_test
--- uart_uvc/tb/uart_test_lib.sv
+++ uart_uvc/tb/uart_test_lib.sv
@@ -1,11 +1,7 @@
-
 class base_test extends uvm_test;
     `uvm_component_utils(base_test)
 
     uart_tb tb; 
-    uart_1_seq seq ;   
-    uvm_objection obj ;
- 
 
     // Constructor
     function new(string name, uvm_component parent);
@@ -14,37 +10,22 @@ class base_test extends uvm_test;
     endfunction
 
     // Build phase
-   function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    `uvm_info("BASE_TEST", "\nBuild phase started", UVM_HIGH)
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        `uvm_info("BASE_TEST", "\nBuild phase started", UVM_HIGH)
 
-    tb = uart_tb::type_id::create("tb", this);
-    `uvm_info("BASE_TEST", "\nEnvironment created", UVM_HIGH)
+        tb = uart_tb::type_id::create("tb", this);
+        `uvm_info("BASE_TEST", "\nEnvironment created", UVM_HIGH)
 
-     //uvm_config_wrapper::set(this,"tb.env.tx_agent.seqr.run_phase", "default_sequence", uart_1_seq::get_type());
-    
-    uvm_config_wrapper::set(this, "tb.env.tx_agent.seqr.run_phase", "default_sequence", uart_1_seq::get_type());
+        uvm_config_wrapper::set(this,"tb.env.rx_agent.seqr.run_phase", "default_sequence", uart_1_seq::get_type());
+        uvm_config_wrapper::set(this, "tb.env.tx_agent.seqr.run_phase", "default_sequence", uart_1_seq::get_type());
 
-
-    //------------IMPORTANT
-   uvm_config_wrapper::set(this, "tb.env.rx_agent.seqr.run_phase", "default_sequence", uart_1_seq::get_type()); 
-//----------------------------
-
-
-
-    // uvm_config_wrapper::set(this, "","default_sequence",::get_type());
-    `uvm_info("BASE_TEST", "\nDefault sequence configured", UVM_HIGH)
+        `uvm_info("BASE_TEST", "\nDefault sequence configured", UVM_HIGH)
     endfunction
 
-     task run_phase(uvm_phase phase);
-     super.run_phase(phase);
+    task run_phase(uvm_phase phase);
+        super.run_phase(phase);
         `uvm_info("BASE_TEST", "\nStarting sequence", UVM_MEDIUM)
-        seq = uart_1_seq::type_id::create("seq");
-        // phase.raise_objection(this);
-
-        // seq.start(tb.env.tx_agent.seqr);
-        // phase.drop_objection(this);
-        
     endtask
 
     function void end_of_elaboration_phase(uvm_phase phase);
@@ -52,9 +33,38 @@ class base_test extends uvm_test;
         `uvm_info("BASE_TEST", "End of elaboration phase", UVM_HIGH)
         uvm_top.print_topology();
     endfunction
-
 endclass
 
+class uart_parity_test extends base_test;
+    `uvm_component_utils(uart_parity_test)
 
+    // Constructor
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+        `uvm_info(get_type_name(), "\nNew phase ", UVM_HIGH)
+    endfunction
 
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase); // Reuse base_test build_phase
+        `uvm_info(get_type_name(), "\nBuild phase (extended) started", UVM_HIGH)
+      uvm_config_wrapper::set(this,"tb.env.rx_agent.seqr.run_phase", "default_sequence", uart_parity_seqs::get_type());
+        uvm_config_wrapper::set(this, "tb.env.tx_agent.seqr.run_phase", "default_sequence", uart_parity_seqs::get_type());
+    endfunction
+endclass
 
+class uart_rand_dual_send_test extends base_test;
+    `uvm_component_utils(uart_rand_dual_send_test)
+
+    // Constructor
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+        `uvm_info(get_type_name(), "\nNew phase ", UVM_HIGH)
+    endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase); // Reuse base_test build_phase
+        `uvm_info(get_type_name(), "\nBuild phase (extended) started", UVM_HIGH)
+      uvm_config_wrapper::set(this,"tb.env.rx_agent.seqr.run_phase", "default_sequence", uart_rand_dual_send_seqs::get_type());
+        uvm_config_wrapper::set(this, "tb.env.tx_agent.seqr.run_phase", "default_sequence", uart_rand_dual_send_seqs::get_type());
+    endfunction
+endclass
