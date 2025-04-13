885eb7c795c105a19e523e6f618d5da370f608df
RedaM4 <xxred49xx@gmail.com>
2025-04-13 11:08:03 +0300

    uart_0

---
+++ uart_ver/tb/run.f
@@ -0,0 +1,4 @@
+// +UVM_VERBOSITY=UVM_FULL 
+// +ntb_random_seed_automatic
+// +UVM_TESTNAME=base_test
+
---
+++ wb_soc_rtl/filelist.f
@@ -0,0 +1,23 @@
+// SPI files
+spi/fifo4.v
+spi/simple_spi_top.v
+
+// UART files
+uart/raminfr.v
+uart/uart_defines.v
+uart/uart_sync_flops.v
+uart/uart_rfifo.v
+uart/uart_tfifo.v
+uart/uart_receiver.v
+uart/uart_transmitter.v
+uart/uart_regs.v
+uart/uart_wb.v
+uart/uart_top.v
+
+// Wishbone Interconnect files
+WishboneInterconnect/wb_mux.v
+WishboneInterconnect/wb_intercon.sv
+
+// SoC Top
+wb_soc_top.sv
+
