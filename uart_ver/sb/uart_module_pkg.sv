a127858e2dc0a420c6438375da0989c336c91d4f
RedaM4 <xxred49xx@gmail.com>
2025-04-13 13:41:29 +0300

    uart_2

---
+++ uart_ver/sb/uart_module_pkg.sv
@@ -0,0 +1,14 @@
+package uart_module_pkg;
+
+  import uvm_pkg::*;
+`include "uvm_macros.svh"
+
+
+import uart_pkg::*;
+import wb_pkg::*;
+
+`include "uart_ver_ref_module.sv"
+`include "uart_ver_scoreboard.sv"
+`include "uart_ver_ref_env.sv"
+
+endpackage
