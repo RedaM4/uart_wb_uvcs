10fa8ebfd9fd3e0de81f0d1b335494257db86669
CenemaX <eng.raid@protonmail.com>
2025-04-15 23:47:08 -0700

    Wrote 5 test cases for uart (needs testing)

--- wb_uvc/sv/wb_master_seqs.sv
+++ wb_uvc/sv/wb_master_seqs.sv
@@ -42,46 +42,112 @@ endclass : wb_master_sequence
 
 
 
-class five_uart_random extends wb_master_sequence;
+class uart_ten_random extends wb_master_sequence;
   
   // Required macro for sequences automation
-  `uvm_object_utils(five_uart_random)
+  `uvm_object_utils(uart_ten_random)
 
   // Constructor
-  function new(string name="five_uart_random");
+  function new(string name="uart_ten_random");
     super.new(name);
   endfunction
 
   // Sequence body definition
   virtual task body();
-    `uvm_info(get_type_name(), "Executing 5 random uart calls", UVM_LOW)
-     repeat(5)
+    `uvm_info(get_type_name(), "Executing 10 random uart calls", UVM_LOW)
+     repeat(10)
       `uvm_do(req)
   endtask
   
-endclass : five_uart_random
+endclass : uart_ten_random
 
-/*
-class yapp_012_seq extends yapp_base_seq;
+
+
+
+class uart_five_write_five_read extends wb_master_sequence;
   
   // Required macro for sequences automation
-  `uvm_object_utils(yapp_012_seq)
+  `uvm_object_utils(uart_five_write_five_read)
 
   // Constructor
-  function new(string name="yapp_012_seq");
+  function new(string name="uart_five_write_five_read");
     super.new(name);
   endfunction
 
   // Sequence body definition
   virtual task body();
-    `uvm_info(get_type_name(), "Executing yapp_012_packet sequence", UVM_LOW)
-      `uvm_do_with(req, {addr==0;});
-      `uvm_do_with(req, {addr==1;});
-      `uvm_do_with(req, {addr==2;});
-  //    `uvm_do_with(req, {addr==3;});
+    `uvm_info(get_type_name(), "send 5 write signals to random addresses of uart, then send 5 read signals from random addresses of uart", UVM_LOW)
+     repeat(5)
+      `uvm_do_with(req, {M_STATE==WRITE;});
+    repeat(5)
+      `uvm_do_with(req, {M_STATE==READ;});
   endtask
+  
+endclass : uart_five_write_five_read
 
 
 
-endclass : yapp_012_seq
-*/
+
+class uart_write_to_all_addresses extends wb_master_sequence;
+  
+  // Required macro for sequences automation
+  `uvm_object_utils(uart_write_to_all_addresses)
+
+  // Constructor
+  function new(string name="uart_write_to_all_addresses");
+    super.new(name);
+  endfunction
+
+  // Sequence body definition
+  virtual task body();
+    `uvm_info(get_type_name(), "sending write signal to all uart addresses from 32 to 63", UVM_LOW)
+    for(int i=32; i<64; i++)begin 
+      `uvm_do_with(req, {address==i;M_STATE==WRITE;});
+    end
+  endtask
+  
+endclass : uart_write_to_all_addresses
+
+
+
+class uart_read_from_all_addresses extends wb_master_sequence;
+  
+  // Required macro for sequences automation
+  `uvm_object_utils(uart_read_from_all_addresses)
+
+  // Constructor
+  function new(string name="uart_read_from_all_addresses");
+    super.new(name);
+  endfunction
+
+  // Sequence body definition
+  virtual task body();
+    `uvm_info(get_type_name(), "sending read signal to all uart addresses from 32 to 63", UVM_LOW)
+    for(int i=32; i<64; i++)begin 
+      `uvm_do_with(req, {address==i;M_STATE==READ;});
+    end
+  endtask
+  
+endclass : uart_read_from_all_addresses
+
+
+
+
+class uart_sit_idle_for_10 extends wb_master_sequence;
+  
+  // Required macro for sequences automation
+  `uvm_object_utils(uart_sit_idle_for_10)
+
+  // Constructor
+  function new(string name="uart_sit_idle_for_10");
+    super.new(name);
+  endfunction
+
+  // Sequence body definition
+  virtual task body();
+    `uvm_info(get_type_name(), "sitting IDLE, no read or write for 10 times", UVM_LOW)
+    repeat(10)
+      `uvm_do_with(req, {M_STATE==IDLE;});
+  endtask
+  
+endclass : uart_sit_idle_for_10
