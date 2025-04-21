479e06c187ac21ac2884bea84e7cd40019294b07
CenemaX <eng.raid@protonmail.com>
2025-04-21 09:02:52 -0700

    ...

--- wb_uvc/sv/wb_master_seqs.sv
+++ wb_uvc/sv/wb_master_seqs.sv
@@ -56,7 +56,7 @@ class wb_write_seq extends wb_master_sequence;
   // Sequence body definition
   virtual task body();
     `uvm_info(get_type_name(), "Writing data to wishbone on an address", UVM_LOW)
-      `uvm_do_with(req, req.address == this.addr; req.data==this.data; req.M_STATE==WRITE;)
+      `uvm_do_with(req, {req.address == this.addr; req.data==this.data; req.M_STATE==WRITE;})
   endtask
   
 endclass : wb_write_seq
